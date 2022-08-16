8d78c37bd9eaecfd3cccc4631cd9ddf7eedbed4f
rikoPro <atalayevrim.ea@gmail.com>
2022-08-16 08:32:12 +0300

    Add project files.

---
+++ TestUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace TestUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine(item.DailyPrice);
+            }
+            var car = carManager.GetById(2);
+            Console.WriteLine(car.Description);
+        }
+    }
+}
